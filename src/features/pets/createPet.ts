045af61cfd9619a35623d80924d61b51edbb18e2
jonathonflorek <jonathonflorek@gmail.com>
2020-03-09 20:22:50 -0600

    add schema checking

--- src/features/pets/createPet.ts
+++ src/features/pets/createPet.ts
@@ -2,10 +2,34 @@ import * as express from 'express';
 import { getRepository, QueryFailedError } from 'typeorm';
 import { PetEntity } from '../../models/PetEntity';
 
+import * as t from 'io-ts';
+import { either } from 'fp-ts';
+import { failure } from 'io-ts/lib/PathReporter';
+
+const petCreation = t.strict({
+    pet: t.strict({
+        name: t.string,
+        type: t.string,
+        breed: t.string,
+        location: t.strict({
+            type: t.literal('Point'),
+            coordinates: t.tuple([t.number, t.number]),
+        }),
+    }),
+});
+
 export async function createPet(req: express.Request, res: express.Response) {
+    const petCreate = petCreation.decode(req.body);
+
+    if (either.isLeft(petCreate)) {
+        return res.status(422).json({
+            errors: failure(petCreate.left),
+        });
+    }
+
     const repository = getRepository(PetEntity);
     try {
-        const ir = await repository.insert(req.body.pet || {});
+        const ir = await repository.insert(petCreate.right.pet);
         res.status(201).json({
             pet: {
                 id: ir.identifiers[0].id,
--- test/integration/pets/createPet.test.ts
+++ test/integration/pets/createPet.test.ts
@@ -3,13 +3,41 @@ import chaiHttp = require('chai-http');
 import { getRepository } from 'typeorm';
 import { PetEntity } from '../../../src/models/PetEntity'
 import { app } from '../../../src';
-import { CONFLICT, CREATED } from 'http-status-codes';
+import { CONFLICT, CREATED, UNPROCESSABLE_ENTITY } from 'http-status-codes';
 
 chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Endpoint test for pets: POST /pets', () => {
     const PETS_ENDPOINT = '/pets';
+
+    it('GIVEN an invalid pet, ' +
+        'WHEN posting to pets endpoint, ' +
+        'THEN HTTP STATUS UNPROCESSABLE_ENTITY and an errors list',
+        async () => {
+
+            // Arrange
+
+            const pet = {
+                name: 'Mysty',
+                // missing other fields
+            };
+
+            // Act
+
+            const result = await chai
+                .request(app)
+                .post(PETS_ENDPOINT)
+                .send({ pet });
+            const allPets = await getRepository(PetEntity).find();
+
+            // Assert
+
+            expect(result).to.have.status(UNPROCESSABLE_ENTITY);
+            expect(allPets).to.have.length(0);
+            expect(result.body.errors.length).to.be.greaterThan(0);
+
+        })
 
     it('GIVEN a valid pet, ' +
         'WHEN posting to pets endpoint, ' +
